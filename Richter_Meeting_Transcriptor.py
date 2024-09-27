74f9ecf4513e2fe49de2068cb51be29e0607b3ca
khezamiyoussef <youssefkhz17@gmail.com>
2024-09-27 14:55:20 +0200

    Add files via upload

---
+++ Richter_Meeting_Transcriptor.py
@@ -0,0 +1,35 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Richter Meeting Transcriptor",
+    page_icon="🗣️",
+)
+
+st.image("richter_logo.jpeg")
+
+
+
+
+st.write("# Welcome to Richter Meeting Transcriptor! 👋")
+
+st.sidebar.success("Hello There  👀")
+
+st.markdown(
+    """
+    The company's meeting transcriber is an advanced tool designed to enhance communication and productivity during discussions. Equipped with a range of features, it seamlessly captures real-time conversations, converting speech to text with remarkable accuracy. Users can easily highlight key points, tag participants, and create actionable items directly within the transcript. Additionally, the platform supports multiple languages and integrates effortlessly with existing communication tools, ensuring that all team members, regardless of their location, can stay aligned and informed.
+      
+"""
+)
+
+
+st.write("") 
+st.write("") 
+
+st.markdown(
+     """
+ ### Want to learn more?
+    - Check out https://www.richter-lt.de
+    - Ask a question here https://www.richter-lt.de/contact-us
+    
+"""
+)
